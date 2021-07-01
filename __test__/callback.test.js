eefdbd8df1df4d653d179092ba0852cbb8ebd8c1
Francisco Lopez <franlopbri@gmail.com>
2021-06-30 23:07:56 -0500

    callback

---
+++ __test__/callback.test.js
@@ -0,0 +1,11 @@
+import { callbackHell } from '../callback';
+
+describe('Probando un callback', () => {
+    test('Callback', done => {
+        function otherCallback(data){
+            expect(data).toBe('Hola Javascripters');
+            done();
+        }
+        callbackHell(otherCallback);
+    });
+});
