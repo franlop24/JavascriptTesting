703778d762d7cbc16b618259f4503829b6865b93
Francisco Lopez <franlopbri@gmail.com>
2021-07-01 18:28:50 -0500

    Excepciones con Snapshots

--- __test__/snapshot.test.js
+++ __test__/snapshot.test.js
@@ -1,8 +1,27 @@
 import { getCharacter } from '../snapshot';
 import rick from '../rick.json';
+import { expect, test } from '@jest/globals';
 
 describe('Es hora de las instantaneas', () => {
     test('Snapshot', () => {
         expect(getCharacter(rick)).toMatchSnapshot();
+    });
+
+    // test('Siempre fallará la instantanea', () => {
+    //     const user = {
+    //         createdAt: new Date(),
+    //         id: Math.floor(Math.random() * 20)
+    //     };
+    //     expect(user).toMatchSnapshot();
+    // });
+
+    test('Tenemos una axception dentro del código', () => {
+        const user = {
+            id: Math.floor(Math.random() * 20),
+            name: "Francisco Lopez"
+        };
+        expect(user).toMatchSnapshot({
+            id: expect.any(Number)
+        });
     });
 });
