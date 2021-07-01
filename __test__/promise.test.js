1f0829271b235ed31c439b723e176000f0d665d5
Francisco Lopez <franlopbri@gmail.com>
2021-07-01 15:18:53 -0500

    Reject y Resolve

--- __test__/promise.test.js
+++ __test__/promise.test.js
@@ -8,6 +8,13 @@ describe('Probando promises', () => {
             expect(data.results.length).toBeGreaterThan(0);
             done();
         });
+    });
 
+    test('Resuelve un Hola!', () => {
+        return expect(Promise.resolve('Hola!')).resolves.toBe('Hola!');
+    });
+
+    test('Rechaza con un error', () => {
+        return expect(Promise.reject('Errror')).rejects.toBe('Errror');
     });
 });
